dafe0b74413fc3987bca1bfc3e2aa4ab9113fed8
ishantk <er.ishant@gmail.com>
2025-06-26 11:22:00 +0530

    functions and recursion

---
+++ Session8B.py
@@ -0,0 +1,18 @@
+# for number in range(1, 11):
+#     print(number)
+
+# Functions - replacing loops
+# Recursion - executing a function again and again
+# execution must stop on some condition
+
+def print_number(number):
+    print(number)
+    # if number < 10:
+    #     print_number(number=number+1)
+
+    if number > 1:
+        print_number(number=number-1)
+
+
+print_number(number=10)
+
