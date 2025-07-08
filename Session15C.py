09595e7ddca30b68feebb8e8de76592e49d8cfdf
ishantk <er.ishant@gmail.com>
2025-07-08 11:20:18 +0530

    Linked List Assignment

---
+++ Session15C.py
@@ -0,0 +1,24 @@
+"""
+    Assignment: Shopping Cart as Circular Doubly LinkedList
+    Scenario: Amazon eCommerce WebApp
+"""
+
+# Product will have quantity and price as 2 important attributes, 
+# other than you choose
+class Product:
+    pass
+
+# L1
+# Circular Doubly Linked List
+# function -> update quantity of product
+#             if quantity is updated as 0 -> delete from list
+
+# L2
+# Sorting -> Price/Qunatity High to Low or Low to High
+# Search
+# Filter
+class ShoppingCart:
+    pass
+
+# create a function, to apply promo code on the list of products
+# and give suitable discounts
