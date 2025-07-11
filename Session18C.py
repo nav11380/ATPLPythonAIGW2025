e8cc5958b519dec4ab9b16c62ab97829c9993a7d
ishantk <er.ishant@gmail.com>
2025-07-11 11:50:12 +0530

    Visitor Log App with FileHandling and OOPS

--- Session18C.py
+++ Session18C.py
@@ -2,12 +2,24 @@
 Application
 """
 
+# Model
+from Session18A import Visitor
+
+# Controller
+from Session18B import FileHelper
+
+# View + Controller
 class VisitorApp:
 
     def __init__(self):
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print("Welcome to Visitor Log Book App")
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        
+        # Create a File Helper Object, which is an attribute of VistorApp
+        # Has-A Relationship
+        self.file_helper = FileHelper()
+        print('File Size:', self.file_helper.file_size(), 'bytes')
 
     def show_menu(self):
         
@@ -19,13 +31,26 @@ class VisitorApp:
             print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             choice = int(input('Enter Your Choice: '))
 
-
             if choice == 1:
-                print("Add A new Visitor Log..")
+                self.add_visitor()
             elif choice == 2:
-                print("Read All Visitor Logs..")
+                self.view_all_visitors()
             elif choice == 3:
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                print('Thank You for Using Visitor App')
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
                 break
             else:
                 print("Invalid Menu Option")
 
+    def add_visitor(self):
+        # 1. Create an Empty Visitor Object
+        visitor = Visitor()
+        # 2. Take Input
+        visitor.input_visitor_details()
+        # 3. Save Visitor in File
+        visitor.serial_no = self.file_helper.lines_in_file()
+        self.file_helper.write_in_file(content=str(visitor))
+
+    def view_all_visitors(self):
+        self.file_helper.read_file()
