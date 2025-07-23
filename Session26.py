94cdc0e3df0e1a4cebe4c3e0dbf6fcce02645cb1
ishantk <er.ishant@gmail.com>
2025-07-23 11:10:49 +0530

    Add Patient Flow

---
+++ Session26.py
@@ -0,0 +1,89 @@
+from flask import *
+from Session25A import User
+from Session24 import MongoDBHelper
+import hashlib
+
+web_app = Flask('Doctors App')
+db = MongoDBHelper()
+db.select_db(db_name='gw2025', collection='users')
+
+# View
+# HW:Validation of the Form: 
+# https://www.w3schools.com/bootstrap5/bootstrap_form_validation.php
+@web_app.route('/')
+def index():
+    return render_template('index.html')
+
+# View
+@web_app.route('/register')
+def register():
+    return render_template('register.html')
+
+# View
+@web_app.route('/home')
+def home():
+    return render_template('home.html', name=session['name'], email=session['email'])
+
+# View
+@web_app.route('/add-patient')
+def add_patient():
+    return render_template('add-patient.html', name=session['name'], email=session['email'])
+
+
+# Controller
+@web_app.route('/add-user', methods=['POST'])
+def add_user_in_db():
+    user = User()
+    user.name = request.form['name']
+    user.email = request.form['email']
+    # encryption
+    user.password = hashlib.sha256(request.form['password'].encode('utf-8')).hexdigest()
+    user.show()
+
+    result = db.insert(document=user.to_document())
+
+    if len(str(result.inserted_id)) > 0:
+        # Session is used to save the data whenerver you need it
+        # across the entire application
+        # this session object will be accessible anywhere in Flask Python Code
+        session['user_id'] = str(result.inserted_id)
+        session['name'] = user.name
+        session['email'] = user.email
+
+        # we can even save the entire user document/dictioanry in session
+        # session['user'] = user.to_document()
+
+        return render_template('home.html', name=user.name, email=user.email)
+    else:
+        return 'Something Went Wrong. Please Try Again'
+
+@web_app.route('/fetch-user', methods=['POST'])
+def fetch_user_from_db():
+    query = {
+        'email': request.form['email'],
+        'password': hashlib.sha256(request.form['password'].encode('utf-8')).hexdigest()
+    }
+
+    documents = db.fetch(query)
+
+    user = documents[0] # retrieved from MongoDB and it is a dictionary
+    # print(user)
+
+    if len(documents) > 0:
+        session['user_id'] = str(user['_id'])
+        session['name'] = user['name']
+        session['email'] = user['email']
+        return render_template('home.html', name=user['name'], email=user['email'])
+    else:
+        return 'Username or Password Invalid. Please Try Again'
+   
+
+def main():
+    # Secret Key, we have to create manually of our choice
+    # It is required for Session Management
+    web_app.secret_key = 'doctors-app-key-v1'
+    # web_app.run()
+    web_app.run(port=5001)
+
+if __name__ == '__main__':
+    main()
---
+++ Session26A.py
@@ -0,0 +1,26 @@
+# Patient Object: name, phone, email, address, gender, age, weight, bp, sugar, temperature, allergies
+class Patient:
+
+    def __init__(self, name, phone, email, address, 
+                 gender, age, weight, 
+                 bp, sugar, temperature, allergies, doctor_id):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.gender = gender
+        self.age = age
+        self.weight = weight
+        self.bp = bp
+        self.sugar = sugar
+        self.temperature = temperature
+        self.allergies = allergies
+        self.doctor_id = doctor_id
+        
+    def to_document(self):
+        return vars(self)
+    
+    # now, its not required as we have web interfaces
+    # if you want to print, you can use to_document above
+    def show(self):
+        pass
