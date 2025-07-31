cc86e285d96f5cac18fae247de9bd3a1c025ce8b
ishantk <er.ishant@gmail.com>
2025-07-31 11:40:40 +0530

    Agentic AI with OPEN AI

---
+++ AhenticAISession2B.py
@@ -0,0 +1,54 @@
+# Function tool - Documentation 
+# https://platform.openai.com/docs/guides/function-calling?api-mode=responses
+
+from openai import OpenAI
+
+def ai_response(user_message):
+    
+    tools = [
+            {
+                "type": "function",
+                "name": "add_patient",
+                "description": "Add a new patient in database",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "name": {"type": "string"},
+                        "phone": {"type": "string"},
+                        "email": {"type": "string"},
+                        "gender": {"type": "string"},
+                        "age": {"type": "int"},
+                        "symptoms": {"type": "string"}
+                    },
+                    "required": ["name", "phone", "email", "gender", "age"],
+                }
+            },
+            {
+                "type": "function",
+                "name": "save_consultation",
+                "description": "Add a new consultation of a ptient in database",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "phone": {"type": "string"},
+                        "medicines": {"type": "string"},
+                        "remarks": {"type": "string"},
+                    },
+                    "required": ["phone", "medicines"],
+                }
+            }
+
+        ]
+    
+    OPEN_AI_KEY = ""
+    client = OpenAI(api_key=OPEN_AI_KEY)
+    selected_model = 'gpt-4o-mini'
+
+    response = client.responses.create(
+        model=selected_model,
+        input=[
+                {"role": "system", "content": "You are a Doctors Agent who can create patients and save consultations"},
+                {"role": "user", "content": user_message}
+            ],
+        tools=tools
+    )
