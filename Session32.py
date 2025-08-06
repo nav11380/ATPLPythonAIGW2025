8966f0898e56d29e441869cb95136a2ac1911ea6
ishantk <er.ishant@gmail.com>
2025-08-06 18:01:32 +0530

    Python Concepts

---
+++ Session32.py
@@ -0,0 +1,23 @@
+# import agentic.app
+
+# agentic.app.hello()
+# agentic.app.bye()
+
+
+# import agentic.app as kuchbhi
+# kuchbhi.hello()
+# kuchbhi.bye()
+
+# from agentic.app import hello, bye, Product
+# from agentic.app import hello
+# from agentic.app import bye
+# from agentic.app import Product
+
+# hello()
+
+from agentic.app import *
+hello()
+bye()
+
+pref = Product()
+print(pref)
---
+++ Session32A.py
@@ -0,0 +1,3 @@
+print('[Session32A.py] - start')
+print('[Session32A.py]', __name__)
+print('[Session32A.py] - end')
